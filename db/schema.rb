44a90d20adb1fe4f3f3c385e8d669b8e4f814190
RyoTate <r43ke6nt3@gmail.com>
2017-03-31 18:18:06 +0900

    groupsテーブルにnameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322065033) do
+ActiveRecord::Schema.define(version: 20170328131257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text     "body"
@@ -39,4 +45,5 @@ ActiveRecord::Schema.define(version: 20170322065033) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
+
 end
