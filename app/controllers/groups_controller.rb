560109696d77cedeb63ae3a00376f6c6e5b0d9cd
RyoTate <r43ke6nt3@gmail.com>
2017-04-08 19:55:50 +0900

    new,createアクションの作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,24 @@
 class GroupsController < ApplicationController
   def index
   end
+
+  def new
+    @group = Group.new
+    @users = User.all
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:sample_form).permit(users: [])
+  end
+
+  def group_params
+    params.require(:group).permit(:name, { user_ids:[] })
+  end
 end
