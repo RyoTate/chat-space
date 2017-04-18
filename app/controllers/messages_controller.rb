d1331b010d112a1f91e6a7b06de5cbdaf630b1af
RyoTate <r43ke6nt3@gmail.com>
2017-04-18 20:41:40 +0900

    メッセージ送信に必要なアクション

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,26 @@
 class MessagesController < ApplicationController
+  before_action :get_group, only: [:index, :create]
   def index
+    @groups = current_user.groups.includes(:users)
+    @message = Message.new
+    @messages = Message.where(group_id: params[:group_id]).order(created_at: :DESC).includes(:user)
+    @users = @group.users
   end
 
   def new
   end
-end
+
+  def create
+    @message = Message.new(create_params)
+    redirect_to group_messages_path(@group) if @message.save
+  end
+
+  private
+  def create_params
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
+  end
+  def get_group
+    @group = Group.find(params[:group_id])
+  end
