3c91149db531e2e5a2cddbaae930b8d71a99d374
RyoTate <r43ke6nt3@gmail.com>
2017-04-18 22:25:09 +0900

    メッセージが保存されない時のフラッシュメッセージ設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,15 +12,21 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(create_params)
-    redirect_to group_messages_path(@group) if @message.save
+    if @message.save
+      redirect_to group_messages_path(@group)
+    else
+      flash.now[:alert] = "メッセージが保存できませんでした"
+      render action: :index
+    end
   end
+
 
   private
   def create_params
     params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 
-  end
   def get_group
     @group = Group.find(params[:group_id])
   end
+end
