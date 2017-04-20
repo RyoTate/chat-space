3096aa3b326849510238e3d0a2e3cd25783ba7cc
RyoTate <r43ke6nt3@gmail.com>
2017-04-20 23:16:42 +0900

    user_id: :current_user.idの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,7 +20,7 @@ class MessagesController < ApplicationController
 
   private
   def create_params
-    params.require(:message).permit(:body).merge(group_id: params[:group_id],user_id: params[:current_user.id])
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: :current_user.id)
   end
 
   def get_group
