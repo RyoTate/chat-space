8f6be871031da5504e2feec5d437d108486d1669
RyoTate <r43ke6nt3@gmail.com>
2017-05-09 18:38:45 +0900

    メッセージ本文が空の場合に入力を不可にするバリデーション

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
   belongs_to :group
   scope :current_group, -> (group){ where(group_id: group) }
   scope :last_created, -> {order(created_at: :DESC)}
+  validates :body, presence: true
 end
